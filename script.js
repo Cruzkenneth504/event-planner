7fd7432ea71b7cf6711bd482610059193565877d
kenneth o cruz <cruzkenneth504@gmail.com>
2023-04-06 17:54:42 -0700

    Added Js comment Lines

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-//$$(document).ready(function () {} allows us to invoke DOM manipulation//
+//$(document).ready(function () {}; allows us to invoke DOM manipulation//
 $(document).ready(function () {
+  //#ID has properly been added to HTML file to set date format//
+  //.Claas Event listener has been added//
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
   $(".saveBtn").on("click", function () {
     
       console.log(this);
-      var text = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id"); 
-
-      localStorage.setItem(time, text);
+      var text = $(this).siblings(".description").val();//links to html sheet //
+      var time = $(this).parent().attr("id");//links to changes inside style sheet// 
+     
+      localStorage.setItem(time, text);//local storage will save event moments// 
+      
   })
   
-  $("#hour8 .description").val(localStorage.getItem("hour8"));
-  $("#hour9 .description").val(localStorage.getItem("hour9"));
-  $("#hour10 .description").val(localStorage.getItem("hour10"));
-  $("#hour11 .description").val(localStorage.getItem("hour11"));
-  $("#hour12 .description").val(localStorage.getItem("hour12"));
-  $("#hour13 .description").val(localStorage.getItem("hour13"));
-  $("#hour14 .description").val(localStorage.getItem("hour14"));
-  $("#hour15 .description").val(localStorage.getItem("hour15"));
-  $("#hour16 .description").val(localStorage.getItem("hour16"));
-  $("#hour17 .description").val(localStorage.getItem("hour17"));
-
+//This function will be the runninng engine//
   function hourTracker() {
     
       var currentHour = moment().hour(); 
@@ -48,3 +41,14 @@ $(document).ready(function () {
   }
   hourTracker(); 
 })
+
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
